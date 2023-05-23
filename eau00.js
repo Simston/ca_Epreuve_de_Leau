0339ec1441de3077d33d359db2c0ca08ee93f3b4
Simston <32360996+Simston@users.noreply.github.com>
2023-05-23 21:15:31 +0200

    Refactoring eau00.js and eau01.js

--- eau00.js
+++ eau00.js
@@ -2,32 +2,37 @@ const MyTools = require('./MyTools');
 const mytools = new MyTools();
 
 // Recover process.argv array
-var arg = process.argv;
-var booleanVerif = mytools.checkArgumentCount(0, arg);
-console.log(booleanVerif);
+const arg = process.argv;
+let arrayOfCombinaison = [];
 
 // Combinaisons de 3 chiffres
-if(booleanVerif){
+    function threeDigitCombos(){
 
-    for(var i=0; i < 10; i++){
-   
-        //j=i+1 for to avoid repetition
-        for(var j=i+1; j < 10 ; j++){
-    
-            // same for x
-            for(var x=j+1; x < 10; x++) {
-    
-                // 3 digit comparaison
-                if(i !== j && i !== x && j !== x){
-                    
-                    console.log(i + "" + j + "" + x);
-    
-                } 
-            }   
+        let index = 0;
+
+        for(var i=0; i < 10; i++){
+            //j=i+1 for to avoid repetition
+            for(var j=i+1; j < 10 ; j++){
+                // same for x
+                for(var x=j+1; x < 10; x++) {
+                    // 3 digit comparaison
+                    if(i !== j && i !== x && j !== x){
+                        arrayOfCombinaison[index] = i + "" + j + "" + x;
+                        index++;
+                    } 
+                }    
+            }
         }
     }
 
-}else{
-    console.log("Nous n'attendons pas d'arguments ici")
-}
+// Part 1: Error Handling 
+const argsOk = mytools.checkArgumentCount(0, arg);
+if (argsOk === false) console.log("Nous n'attendons pas d'arguments ici.");
 
+// Part 2: Parsing
+
+// Part 3: Resolution # Appel de la ou des fonctions principales qui résolvent mon souci
+if (argsOk) threeDigitCombos();
+
+// Part 4: Display 
+console.log(arrayOfCombinaison.toString());
--- eau01.js
+++ eau01.js
@@ -2,26 +2,36 @@ const MyTools = require('./MyTools');
 const mytools = new MyTools();
 
 // Recover process.argv array
-var arg = process.argv;
-var booleanVerif = mytools.checkArgumentCount(0, arg);
+const arg = process.argv;
+let arrayOfresults = [];
 
-// COmbinaisons de 2 nombres
-var arrayOfresults = [];
-var index = 0;
+// Combinaisons de 2 nombres
+function generateFourDigitCombinations(){
 
-if(booleanVerif) {
-
+    let index = 0;
+    //first Loop
     for (var i=0; i < 10;i++){
-
         for (var y=0; y < 10; y++){
-    
-             arrayOfresults[index] = i + "" + y;
-             index++;
-    
+            //second Loop
+            for(var x=0; x < 10; x++){
+                for(var z=0; z < 10; z++){
+                    arrayOfresults[index] = i + "" + y + " " + x + "" + z;
+                    index++;
+                }
+            }
         }
     }
-}else{
-    console.log("Nous n'attendons pas d'arguments ici");
 }
 
-console.log(arrayOfresults);
+
+// Part 1: Error Handling 
+const argsOk = mytools.checkArgumentCount(0, arg);
+if (argsOk === false) console.log("Nous n'attendons pas d'arguments ici.");
+
+// Part 2: Parsing
+
+// Part 3: Resolution 
+if (argsOk) generateFourDigitCombinations();
+
+// Part 4: Display 
+console.log(arrayOfresults.toString());
