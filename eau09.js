28581504d74ea1f8241a67c47311490eba91496f
Simston <32360996+Simston@users.noreply.github.com>
2023-05-28 20:11:33 +0200

    Update eau09.js

--- eau09.js
+++ eau09.js
@@ -3,25 +3,43 @@ const mytools = new MyTools();
 
 // Recover process.argv array
 const argumentsArray = process.argv;
-const dataUser = process.argv[2];
+const num01 = process.argv[2];
+const num02 = process.argv[3];
+
+function displayMinMax(num1, num2){
+    let result = "";
+
+    if(num2 < num1){
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    for(let i=num1; i < num2; i++){
+        result = result + " " + num1 + " ";
+        num1 ++;
+    }
+    return result;
+}
 
 // Part 1: Error Handling
-const ArgsOk = mytools.checkArgumentCount(1, argumentsArray);
+const ArgsOk = mytools.checkArgumentCount(2, argumentsArray);
 let StringOk;
-let dataUserLength;
-if(dataUser !== undefined){
-  StringOk = mytools.isStringValid(dataUser);
-  dataUserLength = dataUser.length;
+let String2Ok;
+
+if(num01 !== undefined && num02 !== undefined){
+  StringOk = mytools.isStringCountainOnlyNumbers(num01);
+  String2Ok =  mytools.isStringCountainOnlyNumbers(num02);
 } 
 if(!ArgsOk){
-  console.log("Veuillez entrer 1 chaîne de caractères.");
+  console.log("Veuillez entrer 2 nombres");
 }else if(!StringOk){
+    console.log(false);
+}else if(!String2Ok){
     console.log(false);
 }
 
-// Part 2: Parsing
+// Part : Resolution
+let suiteDisplay = displayMinMax(num01,num02);
 
-// Part 3: Resolution
-
-// Part 4: Display
-if(StringOk && ArgsOk) console.log(true);
+// Part: Display
+if(StringOk && ArgsOk) console.log(suiteDisplay);
